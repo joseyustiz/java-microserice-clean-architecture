7f4cc53a862280223b54ca0758eedd6b730fbae8
Jose Yustiz <ext_jangyustiz@falabella.cl>
2021-03-05 11:35:08 -0300

    removed user package

--- src/test/java/com/joseyustiz/kata/cleanarchitecture/user/UserApplicationTests.java
+++ src/test/java/com/joseyustiz/kata/cleanarchitecture/user/UserApplicationTests.java
@@ -1,4 +1,4 @@
-package com.joseyustiz.kata.cleanarchitecture.user.user;
+package com.joseyustiz.kata.cleanarchitecture.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
