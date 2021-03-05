b1a7e95a6bf279e1335c117ebddb79a356c01aa3
Jose Yustiz <ext_jangyustiz@falabella.cl>
2021-03-05 11:34:18 -0300

    Initial project

---
+++ src/main/java/com/joseyustiz/kata/cleanarchitecture/user/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.joseyustiz.kata.cleanarchitecture.user.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joseyustiz/kata/cleanarchitecture/user/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseyustiz.kata.cleanarchitecture.user.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
